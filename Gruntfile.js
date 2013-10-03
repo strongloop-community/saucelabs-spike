89986b25fd279d47e7d065d16fac745b7a90f146
Miroslav Bajtos <miroslav@strongloop.com>
2013-10-03 11:10:44 +0200

    Configured saucelabs tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,46 @@
 module.exports = function(grunt) {
+  var browsers = [{
+      browserName: "firefox",
+      platform: "XP"
+  }, {
+      browserName: "chrome",
+      platform: "linux"
+  }, {
+      browserName: "internet explorer",
+      platform: "WIN8",
+      version: "10"
+  }, {
+      browserName: "iphone",
+      platform: "OSX 10.8",
+      version: "6"
+  }, {
+      browserName: "chrome",
+      platform: "XP"
+  }, {
+      browserName: "internet explorer",
+      platform: "VISTA",
+      version: "9"
+  }];
+
   grunt.initConfig({
     connect: {
       server: {
         options: {
           base: "",
           port: 9999
+        }
+      }
+    },
+    'saucelabs-mocha': {
+      all: {
+        options: {
+          urls: ["http://127.0.0.1:9999/test/index.html"],
+          tunnelTimeout: 5,
+          build: process.env.BUILD_NUMBER,
+          concurrency: 3,
+          browsers: browsers,
+          testname: "mocha tests",
+          tags: ["master"]
         }
       }
     },
@@ -17,4 +53,5 @@ module.exports = function(grunt) {
   }
 
   grunt.registerTask("dev", ["connect", "watch"]);
+  grunt.registerTask("test", ["connect", "saucelabs-mocha"]);
 };
--- test/test.js
+++ test/test.js
@@ -3,4 +3,8 @@ describe('dummy', function() {
   it('fails', function() {
     expect(true).to.equal(false);
   });
+
+  it('does nothing', function() {
+    expect(true).to.equal(true);
+  });
 });
